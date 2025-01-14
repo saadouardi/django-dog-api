d75ecc2aff203749dc78f49785d167bac695bb2a
SaadOuardi <saad.ouardi.developer@gmail.com>
2025-01-14 02:32:22 +0100

    done with serializers

---
+++ dogs/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers # type: ignore
+from .models import Dog, Breed
+
+class BreedSerializer(serializers.ModalSerializer):
+    class Meta:
+        model = Breed
+        fields = '__all__'
+        
+class DogSerializer(serializers.ModelSerializer):
+    breed = BreedSerializer(read_only=True)
+    breed_id = serializers.PrimaryKeyRelatedField(
+        queryset=Breed.objects.all(), source='breeed', write_only=True
+    )
+    
+    class Meta:
+        model = Dog
+        fields = '__all__'
