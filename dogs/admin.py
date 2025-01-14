38d4c0f758df0f5a923407588466cb7eed3b48e0
SaadOuardi <saad.ouardi.developer@gmail.com>
2025-01-14 01:40:12 +0100

    Breed, Dog models are done

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from .models import Dog, Breed
 
-# Register your models here.
+admin.site.register(Dog)
+admin.site.register(Breed)
--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+class Breed(models.Model):
+    TINY = 'Tiny'
+    SMALL = 'Small'
+    MEDIUM = 'Medium'
+    LARGE = 'lARGE'
+    SIZE_CHOICES = [
+        (TINY, 'Tiny'),
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large')
+    ]
+    name = models.CharField(max_length=100, unique=True)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES)
+    friendliness = models.PositiveSmallIntegerField(default=1)
+    trainability = models.PositiveSmallIntegerField(default=1)
+    sheeddingamount = models.PositiveSmallIntegerField(default=1)
+    exerciseneeds = models.PositiveSmallIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class Dog(models.Model):
+    MALE = 'Male'
+    FEMALE = 'Female'
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    ]
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, related_name="dogs")
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    color = models.CharField(max_length=50)
+    favoritefood = models.CharField(max_length=100)
+    favoritetoy = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.name} ({self.breed.name})"
